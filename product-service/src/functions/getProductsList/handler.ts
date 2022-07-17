b0692c086d41f2a80cbbef9ad37b3aed5cc5cdd9
Roman Tarnakin <roman_tarnakin@epam.com>
2022-07-17 14:30:22 +0300

    make use of new middleware and error class in handlers

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,14 +1,13 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 
 import productList from "../../../mock/productList.json";
 import schema from './schema';
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  return formatJSONResponse({
+  return {
     productsList: productList
-  });
+  };
 };
 
 export const main = middyfy(getProductsList);
