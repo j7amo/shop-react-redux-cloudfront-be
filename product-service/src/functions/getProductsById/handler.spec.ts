743faea97e8d34c57d41d9449be9727f30af0889
Roman Tarnakin <roman_tarnakin@epam.com>
2022-07-17 17:41:40 +0300

    add unit test for getProductsById handler

---
+++ product-service/src/functions/getProductsById/handler.spec.ts
@@ -0,0 +1,40 @@
+import { Handler } from "aws-lambda";
+
+import { middyfy } from "@libs/lambda";
+
+jest.mock("../../libs/lambda");
+
+describe('getProductsById', () => {
+  let main: any;
+  let mockedMiddyfy: jest.MockedFn<typeof middyfy>;
+
+  beforeEach(async () => {
+    mockedMiddyfy = jest.mocked(middyfy);
+    mockedMiddyfy.mockImplementation((handler: Handler) => {
+      return handler as never;
+    });
+
+    main = (await import('./handler')).main;
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  it('returns correct response object', async () => {
+    const event = {
+      pathParameters: {
+        productId: 1,
+      }
+    } as any;
+
+  const actual = await main(event);
+  expect(actual).toEqual({
+    "count": 4,
+    "description": "App for finding cheap flights to Mars",
+    "id": 1,
+    "price": 2.4,
+    "title": "Mars For Everyone"
+  });
+  });
+});
