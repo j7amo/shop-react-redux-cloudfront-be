49a3a6cc143a968db99c8f078057aeb6917f173d
Roman Tarnakin <roman_tarnakin@epam.com>
2022-07-17 14:28:54 +0300

    use apiGatewayResponseMiddleware middleware

--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -1,16 +1,16 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import type { APIGatewayProxyEvent, Handler } from "aws-lambda";
 import type { FromSchema } from "json-schema-to-ts";
 
-type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> };
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, any>;
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response: Record<string, unknown>, statusCode: number = 200) => {
   return {
-    statusCode: 200,
+    statusCode,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(response)
+    body: JSON.stringify(response),
   }
 }
